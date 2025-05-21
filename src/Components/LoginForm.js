aa03cf65e312dc7c8e60f0677fd9dcb1ac16c4ea
ftanekha <farai.tanekha@gmail.com>
2025-05-21 02:14:33 +0100

    - add required autoComplete attribute to Input elements

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -3,10 +3,10 @@ import LoginButton from './LoginButton.js'
 function LoginForm({handleLoginFormSubmit, setUsername, setPassword, username, password, loginFormDisplay}){
     return(
         <form id='login-form' onSubmit={handleLoginFormSubmit} style={{display: loginFormDisplay? 'flex': 'none'}}>
-            <input type='text' onChange={(e)=>{
+            <input type='text' autoComplete='on' onChange={(e)=>{
                 setUsername(e.target.value)
             }} value={username}/>
-            <input type='password'  onChange={(e)=>{
+            <input type='password' autoComplete='on' onChange={(e)=>{
                 setPassword(e.target.value)
             }} value={password}/>
             <LoginButton/>
