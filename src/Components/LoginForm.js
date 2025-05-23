f839812ac97c6846c1fa8c87e8b4bef0000aeb4b
ftanekha <farai.tanekha@gmail.com>
2025-05-23 17:15:05 +0100

    - provide login details as login input field placeholders

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -3,12 +3,24 @@ import LoginButton from './LoginButton.js'
 function LoginForm({handleLoginFormSubmit, setUsername, setPassword, username, password, loginFormDisplay}){
     return(
         <form id='login-form' onSubmit={handleLoginFormSubmit} style={{display: loginFormDisplay? 'flex': 'none'}}>
-            <input type='text' autoComplete='on' onChange={(e)=>{
-                setUsername(e.target.value)
-            }} value={username}/>
-            <input type='password' autoComplete='on' onChange={(e)=>{
-                setPassword(e.target.value)
-            }} value={password}/>
+            <input 
+                type='text'  
+                value={username}
+                placeholder='type: random@user.com' 
+                autoComplete='on'
+                onChange={
+                    ({target: {value}})=> setUsername(value)
+                }
+            />
+            <input 
+                type='password' 
+                value={password}
+                placeholder='type: random@password.com'
+                autoComplete='on'
+                onChange={
+                    ({target: {value}})=> setPassword(value)
+                }
+            />
             <LoginButton/>
       </form>
     )
